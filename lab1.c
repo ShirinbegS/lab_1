8cbc3d0f2c48b948c1f20cb2d499c22190d0829e
root <root@LAPTOP-SG2OVKS4>
2024-10-03 13:45:49 +0500

    laba

---
+++ lab1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, x;
+    printf("Enter a = "); scanf("%i", &a);
+    printf("Enter b = "); scanf("%i", &b);
+    printf("Enter c = "); scanf("%i", &c);
+    printf("Enter x = "); scanf("%i", &x);
+    int min = a;
+    if (b < min) {
+        min = b;
+    }
+    if (c < min) {
+        min = c;
+    }
+    if (min <= x &&  min%7 == 0) {
+        printf("%i\n", min);
+    } else {
+        printf("%.2f\n", (float)min/(a+b+c - min));
+    } 
+    
+    return 0;
+}
